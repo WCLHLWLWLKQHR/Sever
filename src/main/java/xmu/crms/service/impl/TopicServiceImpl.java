59821dd6f36876c061d8b51ce004442d27f85d04
luweiw <554220532@qq.com>
2017-12-30 15:32:42 +0800

    new impl

---
+++ src/main/java/xmu/crms/service/impl/TopicServiceImpl.java
@@ -0,0 +1,61 @@
+package xmu.crms.service.impl;
+
+import xmu.crms.entity.SeminarGroupTopic;
+import xmu.crms.entity.Topic;
+import xmu.crms.exception.TopicNotFoundException;
+import xmu.crms.service.TopicService;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class TopicServiceImpl implements TopicService {
+    @Override
+    public Topic getTopicByTopicId(BigInteger topicId) throws TopicNotFoundException, IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public void updateTopicByTopicId(BigInteger topicId, Topic topic) throws TopicNotFoundException, IllegalArgumentException {
+
+    }
+
+    @Override
+    public void deleteTopicByTopicId(BigInteger topicId) throws IllegalArgumentException, TopicNotFoundException {
+
+    }
+
+    @Override
+    public List<Topic> listTopicBySeminarId(BigInteger seminarId) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public BigInteger insertTopicBySeminarId(BigInteger seminarId, Topic topic) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public void deleteSeminarGroupTopicById(BigInteger groupId, BigInteger topicId) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void deleteSeminarGroupTopicByTopicId(BigInteger topicId) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public SeminarGroupTopic getSeminarGroupTopicById(BigInteger topicId, BigInteger groupId) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public List<SeminarGroupTopic> listSeminarGroupTopicByGroupId(BigInteger groupId) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public void deleteTopicBySeminarId(BigInteger seminarId) throws IllegalArgumentException {
+
+    }
+}
